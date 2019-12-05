82233211187fab7894ae2eb7b2ad4994736b46b2
Jaume Delclòs Coll <cosarara97@gmail.com>
2019-12-05 16:28:13 +0100

    clean

--- day5/day5.rb
+++ day5/day5.rb
@@ -19,36 +19,30 @@ def run(program)
     op = instruction % 100
     instruction /= 100
     a, b, c = program[pc+1..pc+4]
+    ap, bp, cp = a, b, c
+
+    if instruction % 10 == 0
+      ap = program[a]
+    end
+    instruction /= 10
+    if instruction % 10 == 0
+      bp = program[b]
+    end
+
     case op
-    when 1
-      if instruction % 10 == 0
-        a = program[a]
-      end
-      instruction /= 10
-      if instruction % 10 == 0
-        b = program[b]
-      end
-      program[c] = a + b
+    when 1 # add
+      program[c] = ap + bp
       pc += 4
-    when 2
-      if instruction % 10 == 0
-        a = program[a]
-      end
-      instruction /= 10
-      if instruction % 10 == 0
-        b = program[b]
-      end
-      program[c] = a * b
+    when 2 # mul
+      program[c] = ap * bp
       pc += 4
-    when 3
+    when 3 # input
       program[a] = read_int
       pc += 2
-    when 4
-      if instruction % 10 == 0
-        a = program[a]
-      end
-      write_int(a)
+    when 4 # output
+      write_int(ap)
       pc += 2
+
     else
       puts 'bad bad'
       puts 'pc ' + pc.to_s
