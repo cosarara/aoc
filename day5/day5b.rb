5509c31bc3b4629c0a98e5469f9dba8d77cc4432
Jaume Delclòs Coll <cosarara97@gmail.com>
2019-12-05 17:05:15 +0100

    increment pc automatically on every parameter read

--- day5/day5b.rb
+++ day5/day5b.rb
@@ -15,66 +15,84 @@ def write_int x
   puts "OUTPUT:", x
 end
 
-def run(program)
-  pc = 0
-
-  while program[pc] != 99 do
-    instruction = program[pc]
-    op = instruction % 100
-    #puts "PC " + pc.to_s + " OP " + op.to_s
-    instruction /= 100
-    a, b, c = program[pc+1..pc+4]
-    ap, bp, cp = a, b, c
-
-    if instruction % 10 == 0
-      ap = program[a]
+class VM
+  def in_param
+    a = out_param
+    if @instruction % 10 == 0
+      a = @program[a]
     end
-    instruction /= 10
-    if instruction % 10 == 0
-      bp = program[b]
-    end
-
-    case op
-    when 1 # add
-      program[c] = ap + bp
-      pc += 4
-    when 2 # mul
-      program[c] = ap * bp
-      pc += 4
-    when 3 # input
-      program[a] = read_int
-      pc += 2
-    when 4 # output
-      write_int(ap)
-      pc += 2
-    when 5 # jit
-      if ap != 0
-        pc = bp
-      else
-        pc += 3
-      end
-    when 6 # jif
-      if ap == 0
-        pc = bp
-      else
-        pc += 3
-      end
-    when 7 # lt
-      program[c] = ap < bp ? 1 : 0
-      pc += 4
-    when 8 # eq
-      program[c] = ap == bp ? 1 : 0
-      pc += 4
-    else
-      puts 'bad bad'
-      puts 'pc ' + pc.to_s
-      puts 'in ' + program[pc].to_s
-      puts 'op ' + op.to_s
-      puts program.to_s
-      break
-    end
+    @instruction /= 10
+    a
   end
-  program
+
+  def out_param
+    a = @program[@pc]
+    @pc += 1
+    a
+  end
+
+  def initialize(program)
+    @program = program
+    @pc = 0
+    @instruction = 0
+
+    while @program[@pc] != 99 do
+      pc = @pc
+      @instruction = out_param
+      op = @instruction % 100
+      puts "PC " + pc.to_s + " OP " + op.to_s
+      @instruction /= 100
+
+      case op
+      when 1 # add
+        ap = in_param
+        bp = in_param
+        c = out_param
+        @program[c] = ap + bp
+      when 2 # mul
+        ap = in_param
+        bp = in_param
+        c = out_param
+        @program[c] = ap * bp
+      when 3 # input
+        a = out_param
+        program[a] = read_int
+      when 4 # output
+        ap = in_param
+        write_int(ap)
+      when 5 # jit
+        ap = in_param
+        bp = in_param
+        if ap != 0
+          @pc = bp
+        end
+      when 6 # jif
+        ap = in_param
+        bp = in_param
+        if ap == 0
+          @pc = bp
+        end
+      when 7 # lt
+        ap = in_param
+        bp = in_param
+        c = out_param
+        program[c] = ap < bp ? 1 : 0
+      when 8 # eq
+        ap = in_param
+        bp = in_param
+        c = out_param
+        program[c] = ap == bp ? 1 : 0
+      else
+        puts 'bad bad'
+        puts 'pc ' + @pc.to_s
+        puts 'in ' + @program[@pc].to_s
+        puts 'op ' + op.to_s
+        puts @program.to_s
+        break
+      end
+    end
+    @program
+  end
 end
 
-run(initial_program)
+VM.new(initial_program)
