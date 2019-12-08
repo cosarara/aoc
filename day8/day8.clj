b6cef21555f2b6d21a2ffd544be50b635ccffa45
Jaume Delclòs Coll <jaume@delclos.com>
2019-12-08 10:11:56 +0100

    day 8 part 2

--- day8/day8.clj
+++ day8/day8.clj
@@ -1,5 +1,7 @@
 #!/usr/bin/env clojure
 
+(require '[clojure.string :as str])
+
 (defn to-int [s] (Integer/parseInt s))
 
 (defn to-ints [s] (map (fn [c] (to-int (str c))) s))
@@ -10,18 +12,37 @@
 (defn count-zeros [layer]
   (count-x layer 0))
 
-(defn day8 [data width height]
+(defn split-layers [data width height]
   (let [layersize (* width height)
-        nlayers (/ (count data) layersize)
-        layers (map (fn [x] (to-ints
-                              (subs data
-                                    (* x layersize)
-                                    (* (+ x 1) layersize))))
-                    (range nlayers))
-        best (apply min-key count-zeros layers)]
-        (* (count-x best 1) (count-x best 2))
-    ))
+        nlayers (/ (count data) layersize)]
+    (map (fn [x] (to-ints
+                   (subs data
+                         (* x layersize)
+                         (* (+ x 1) layersize))))
+         (range nlayers))))
 
-(require '[clojure.string :as str])
+(defn day8 [data width height]
+  (let [layers (split-layers data width height)
+        best (apply min-key count-zeros layers)]
+        (* (count-x best 1) (count-x best 2))))
+
+(defn paint [color previous]
+  (if (= 2 previous) color previous))
+
+(defn pretty [picture]
+  (map (fn [x]
+           (if (= 1 x) "X" " "))
+       picture))
+
+(defn display [picture width]
+  (let [rows (partition width picture)]
+    (doall (map (fn [row] (println (str/join "" row))) rows))))
+
+(defn day8b [data width height]
+  (let [layers (split-layers data width height)
+        picture (reduce (fn [top bottom] (map paint bottom top)) layers)]
+    (display (pretty picture) width)))
+
 (def input (str/trim (slurp "input.txt")))
 (println (day8 input 25 6))
+(day8b input 25 6)
