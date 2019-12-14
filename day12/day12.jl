d352c6734d1badaca18627850a8b503c3f271258
Jaume Delclòs Coll <cosarara97@gmail.com>
2019-12-14 16:17:07 +0100

    day12 julia temp mistakes

--- day12/day12.jl
+++ day12/day12.jl
@@ -1,9 +1,9 @@
 positions = [
-    [-7, -1, 6],
-    [6, -9, -9],
-    [-12, 2, -7],
-    [4, -17, -12]
-    ]
+             -7   -1   6;
+             6    -9  -9;
+             -12   2  -7;
+             4   -17 -12
+            ]
 
 #var positions = [
 #    [-1, 0, 2],
@@ -12,33 +12,57 @@ positions = [
 #    [3, 5, -1],
 #]
 
-velocities = [
-    [0, 0, 0],
-    [0, 0, 0],
-    [0, 0, 0],
-    [0, 0, 0]
-    ]
+positions = [
+             -1 0 2;
+             2 -10 -7
+             4 -8 8
+             3 5 -1
+            ]
 
+velocities = zeros(Int64, 4, 3)
+
+function unit(x)
+    if x == 0
+        0
+    elseif x > 0
+        1
+    else
+        -1
+    end
+end
+
+# 1014
 for step in 1:3
     println("STEP ", step)
     for id in 1:4
         # apply gravity
-        p1 = positions[id]
         for snd in 1:4
             if snd == id
                 continue
             end
-            p2 = positions[snd]
-            for coord in 1:3
-                d = if (p1[coord] == p2[coord])
-                        0
-                    elseif p1[coord] > p2[coord]
-                        -1
-                    else 1
-                    end
-            velocities[id][coord] += d
-            end
+            grav = unit.(positions[snd, :] - positions[id, :])
+            println("hey", id)
+            println(positions[id, :])
+            println(positions[snd, :])
+            println(grav)
+            velocities[id, :] += grav
         end
+       # p1 = positions[id]
+       # for snd in 1:4
+       #     if snd == id
+       #         continue
+       #     end
+       #     p2 = positions[snd]
+       #     for coord in 1:3
+       #         d = if (p1[coord] == p2[coord])
+       #                 0
+       #             elseif p1[coord] > p2[coord]
+       #                 -1
+       #             else 1
+       #             end
+       #     velocities[id][coord] += d
+       #     end
+       # end
     end
     # apply velocity
     global positions += velocities
