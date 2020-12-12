72879535da26e655b2d78cfe2cdd86b781fde854
Jaume Delclòs Coll <jaume@delclos.com>
2020-12-12 17:32:12 +0100

    it werks now

--- 2020/day12/day12.py
+++ 2020/day12/day12.py
@@ -1,23 +1,31 @@
 import pygame
 from math import pi
+import sys
+
+nodraw = '--nodraw' in sys.argv
+
 pygame.init()
 
-size = width, height = 800, 800
-screen = pygame.display.set_mode(size)
-myfont = pygame.font.SysFont('monospace', 20)
-shipw = pygame.image.load("ship.png")
-shipe =  pygame.transform.flip(shipw, True, False)
-shipn =  pygame.transform.rotate(shipw, -90)
-ships =  pygame.transform.rotate(shipw, 90)
+if not nodraw:
+    size = width, height = 800, 800
+    screen = pygame.display.set_mode(size)
+    myfont = pygame.font.SysFont('monospace', 20)
+    shipw = pygame.image.load("ship.png")
+    shipe =  pygame.transform.flip(shipw, True, False)
+    shipn =  pygame.transform.rotate(shipw, -90)
+    ships =  pygame.transform.rotate(shipw, 90)
 
 with open('input') as f:
     inp = f.read().strip().split('\n')
 
 def draw(x, y, px, py, l, f, angles=None):
+    if nodraw:
+        return
     screen.fill((255, 255, 255))
     shipcol = (255, 0, 0)
     drawx = 300
-    drawy = height-100
+    drawy = 100
+    #drawy = height // 2
     black = (0, 0, 0)
     pygame.draw.line(screen, black,
                      (-1000+drawx, drawy),
@@ -35,16 +43,22 @@ def draw(x, y, px, py, l, f, angles=None):
 
     if angles:
         d2r = lambda d : pi * (d/180)
-        start_angle, stop_angle = angles
+        pang, nang = start_angle, stop_angle = angles
+        if (stop_angle < start_angle):
+            stop_angle, start_angle = start_angle, stop_angle
         pygame.draw.arc(screen, (0, 255, 0),
                         (sx-100, sy-100, 200, 200),
                         d2r(start_angle), d2r(stop_angle))
+        ship = pygame.transform.rotate(shipe, nang)
+        shiprect = (sx-4, sy-4, 8, 8)
+        screen.blit(ship, shiprect)
 
-    #pygame.draw.rect(screen, shipcol, (x//1.1+drawx, y//1.1+drawy, 5, 5))
-    # -4 because the rect is 8x8
-    shiprect = (sx-4, sy-4, 8, 8)
-    ship = {'W': shipw, 'E': shipe, 'N': shipn, 'S': ships}[f]
-    screen.blit(ship, shiprect)
+    else:
+        # -4 because the rect is 8x8
+        shiprect = (sx-4, sy-4, 8, 8)
+        ship = {'W': shipw, 'E': shipe, 'N': shipn,
+                'S': ships}[f]
+        screen.blit(ship, shiprect)
     lsurf = myfont.render(l, False, black)
     screen.blit(lsurf, (20, height-120))
     psurf = myfont.render(str((x, y, f)), False, black)
@@ -65,29 +79,22 @@ for l in inp:
     b = int(l[1:])
     if a == 'F':
         a = f
+    ang = {'W': 180, 'N': 90, 'E': 0, 'S': 270}
+    face = {v: k for k, v in ang.items()}
+    v = (0, 0)
     if a in 'RL':
         pf = f
-        b = b % 360
-        if a == 'L':
-            360 - b
-        for i in range(b//90):
-            print(f)
-            f = {
-                'W': 'N',
-                'N': 'E',
-                'E': 'S',
-                'S': 'W'
-            }[f]
-        print('rotated from '+pf+' to '+f)
-        #ang = lambda d: {'W': 180, 'N': 90, 'E': 0, 'S': 270}[d]
-        #angles = ang(pf), ang(f)
-        #for a in range(angles[0], angles[1]):
-        #    tmpangles = a, angles[1]
-        #    #if a == 'L':
-        #    #    tmpangles = angles[1], a
-        #    draw(x, y, px, py, l, pf, tmpangles)
+        pang = ang[f]
+        nang = pang
+        for d in range(b):
+            nang += 1 if a == 'L' else -1
+            draw(x, y, px, py, l, f, (pang, nang))
+            #pygame.time.wait(30)
+        print('pf', f)
+        f = face[(nang+360) % 360]
+        print('f', f)
         draw(x, y, px, py, l, f)
-        #pygame.time.wait(2000)
+        #pygame.time.wait(5000)
         continue
     elif a == 'N':
         v = (0, 1)
@@ -110,6 +117,6 @@ for l in inp:
 
     print(l, x, y)
     draw(x, y, px, py, l, f)
-    pygame.time.wait(50)
+    #pygame.time.wait(50)
 
 print(x, y, abs(x)+abs(y))
