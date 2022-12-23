fc01e83f76181e66a5b6865e66fc4883d5643f1b
Jaume Delclòs Coll <jaume.delclos@prima.it>
2022-12-23 11:42:26 +0100

    day 22 fixed part a, did part b

--- 2022/22/b.py
+++ 2022/22/b.py
@@ -7,6 +7,73 @@ def make_map_path(text):
     path = re.findall(r'\d+|\D', lines[-1])
     return map, path
 
+# sections are as follows:
+#   12
+#   3
+#  45
+#  6
+#
+
+
+def wrap_down(x, y):
+    if x < 50: # down from 6
+        print('wrap down from 6 to 2')
+        # we go to 2, with no rotation
+        return (x+100, 0, 'v')
+    if x < 100: # down from 5
+        print('wrap down from 5 to 6')
+        # we go to the right side of 6, and rotate right to face left
+        return (49, 150+(x-50), '<')
+    if x < 150: # down from 2
+        print('wrap down from 2 to 3')
+        # we go to the right side of 3, and rotate right to face left
+        return (99, 50+(x-100), '<')
+    raise Exception('unreachable')
+
+def wrap_right(x, y):
+    if y < 50: # right from 2 to 5, flipping
+        print('wrap right from 2 to 5')
+        return (99, 149-y, '<')
+    if y < 100: # right from 3 to 2, going up now
+        print('wrap right from 3 to 2')
+        return (100+y-50, 49, '^')
+    if y < 150: # right from 5 to 2, flipping
+        print('wrap right from 5 to 2')
+        return (149, 49-(y-100), '<')
+    if y < 200: # right from 6 to 5
+        print('wrap right from 6 to 5')
+        return (y-150+50, 149, '^')
+
+    raise Exception('unreachable')
+
+def wrap_up(x, y):
+    if x < 50: # up from 4 to 3
+        print('wrap up from 4 to 3')
+        return (50, 50+x, '>')
+    if x < 100: # up from 1 to 6
+        print('wrap up from 1 to 6')
+        return (0, x-50+150, '>')
+    if x < 150: # up from 2 to 6
+        print('wrap up from 2 to 6')
+        return (x-100, 199, '^')
+    raise Exception('unreachable')
+
+def wrap_left(x, y):
+    if y < 50: # from 1 to 4, flipping
+        print('wrap left from 1 to 4')
+        return (0, 149-y, '>')
+    if y < 100: # from 3 to 4
+        print('wrap left from 3 to 4')
+        return (y-50, 100, 'v')
+    if y < 150: # from 4 to 1, flipping
+        print('wrap left from 4 to 1')
+        return (50, 49-(y-100), '>')
+    if y < 200: # from 6 to 1
+        print('wrap left from 6 to 1')
+        return (y-150+50, 0, 'v')
+
+    raise Exception('unreachable')
+
 def move(pos, facing, map):
     def in_grid(x, y):
         return (0 <= y < len(map) and
@@ -18,29 +85,41 @@ def move(pos, facing, map):
     if facing == '>':
         x += 1
         if not in_grid(x, y):
-            x = next(x for x, c in enumerate(map[y]) if in_grid(x, y))
+            #x = next(x for x, c in enumerate(map[y]) if in_grid(x, y))
+            print(x, y)
+            x, y, facing = wrap_right(x-1, y)
+            print('->', x, y)
             wrapped = True
-        return (x, y), wrapped
+        return (x, y, facing), wrapped
     elif facing == '<':
         x -= 1
         if not in_grid(x, y):
-            x = next(x for x, c in reversed(list(enumerate(map[y])))
-                     if in_grid(x, y))
+            #x = next(x for x, c in reversed(list(enumerate(map[y])))
+            #         if in_grid(x, y))
+            print(x, y)
+            x, y, facing = wrap_left(x+1, y)
+            print('->', x, y)
             wrapped = True
-        return (x, y), wrapped
+        return (x, y, facing), wrapped
     elif facing == '^':
         y -= 1
         if not in_grid(x, y):
-            y = next(y for y, c in reversed(list(enumerate(map))) if
-                     in_grid(x, y))
+            #y = next(y for y, c in reversed(list(enumerate(map))) if
+            #         in_grid(x, y))
+            print(x, y)
+            x, y, facing = wrap_up(x, y+1)
+            print('->', x, y)
             wrapped = True
-        return (x, y), wrapped
+        return (x, y, facing), wrapped
     elif facing == 'v':
         y += 1
         if not in_grid(x, y):
-            y = next(y for y, c in enumerate(map) if in_grid(x, y))
+            #y = next(y for y, c in enumerate(map) if in_grid(x, y))
+            print(x, y)
+            x, y, facing = wrap_down(x, y-1)
+            print('->', x, y)
             wrapped = True
-        return (x, y), wrapped
+        return (x, y, facing), wrapped
 
 def test():
     with open('example') as exf:
@@ -172,6 +251,7 @@ def wait_input():
                 sys.exit(0)
             elif event.type == pg.KEYDOWN:
                 return
+        clock.tick(100)
 
 def run(map, path):
     start = None
@@ -192,13 +272,21 @@ def run(map, path):
             steps = int(action)
             for step in range(steps):
                 next_pos, wrapped = move(pos, facing, map)
-                nx, ny = next_pos
-                if map[ny][nx] == ' ':
+                nx, ny, nfacing = next_pos
+                if ny > len(map) or nx > len(map[ny]) or map[ny][nx] == ' ':
+                    print('bugged', x, y,
+                                    facing, nx, ny)
+                                    #map[nx][ny])
+
+                    draw(map, history, pos,
+                         f'facing: {facing}\naction: {action}\nnext: {"".join(path[naction+1:][:1])}')
+                    wait_input()
                     raise Exception('bugged', x, y,
-                                    facing, nx, ny,
-                                    map[nx][ny])
+                                    facing, nx, ny)
+                                    #map[nx][ny])
                 if map[ny][nx] == '.':
-                    pos = next_pos
+                    pos = (x, y) = (nx, ny)
+                    facing = nfacing
                     #print('moved to', pos)
                 else:
                     #print('hit a wall')
@@ -228,8 +316,8 @@ def run(map, path):
     facev = {'>': 0, 'v': 1, '<':2, '^':3}[finalfacing]
     print(facev)
     print((finalpos[1]+1)*1000 + (finalpos[0]+1)*4 + facev)
-    while True:
-        draw(map, history, pos)
+    draw(map, history, pos)
+    wait_input()
     return history, pos, facing
 
 if __name__ == "__main__":
