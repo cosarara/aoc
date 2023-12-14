221a6bb545feadb51a409638355dc44ba596a651
Jaume Delclòs Coll <jaume@delclos.com>
2023-12-14 21:12:06 +0100

    idk if it's lcm or just multiplication who cares

--- 2023/08/b.py
+++ 2023/08/b.py
@@ -1,5 +1,6 @@
 import sys
 from itertools import cycle
+from math import lcm
 
 with open(sys.argv[1]) as f:
     text = f.read().strip().split('\n')
@@ -12,13 +13,15 @@ for node_line in text[2:]:
     c = node_line[12:15]
     map[a] = (b, c)
 
-nodes = [n for n in map if n[2] == "A"]
-inst = cycle(instructions)
-steps = 0
-while [n for n in nodes if n[2] != "Z"]:
-    dir = 0 if next(inst) == "L" else 1
-    nodes = [map[n][dir] for n in nodes]
-    #print(nodes)
-    steps += 1
+def cycles(n):
+    inst = cycle(instructions)
+    steps = 0
+    while n[2] != "Z":
+        dir = 0 if next(inst) == "L" else 1
+        n = map[n][dir]
+        steps += 1
+    return steps
 
-print(steps)
+node_cycles = [cycles(n) for n in map if n[2] == "A"]
+print(node_cycles)
+print(lcm(*node_cycles))
